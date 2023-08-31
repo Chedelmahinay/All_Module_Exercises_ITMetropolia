73bda9dd946e232b818a1a3b8adcc4b8d3dd43f0
Chedel Castro Mahinay <Mahinayyychedel@gmail.com>
2023-08-31 21:22:22 +0300

    For Teacher Amir and Chau

--- Module 3.py
+++ Module 3.py
@@ -65,4 +65,4 @@ except: print("Not a number.");
 if year % 4 == 0 or year % 400 == 0:
     print("It's a leap year")
 else:
-    print("Nahh bruhh, even Michael Jordan cannot leap this year")
+    print("It is not a leap year")
